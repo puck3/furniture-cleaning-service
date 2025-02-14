b6d695a6591c0162cc0f39ac78fe1b8b96161c59
puck3 <shipilovk04@gmail.com>
2025-02-14 21:35:57 +0300

    add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,28 @@
-export default function NotFound() {
-  return <div>404</div>;
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-16">
+      <Image
+        src="/page-not-found.png"
+        alt="Страница не найдена"
+        width={1024}
+        height={1024}
+        className="sm:w-[100%] md:w-[70%] lg:w-[50%] 2  xl:w-[30%] mb-6"
+      />
+      <h1 className="text-3xl font-bold mb-4">Страница не найдена</h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Кажется, здесь ничего нет. Но мы можем почистить ваш диван!
+      </p>
+      <Link
+        href="/"
+        className="px-6 py-3 text-white bg-fuchsia-600 rounded-lg hover:bg-blue-700 transition"
+      >
+        На главную
+      </Link>
+    </div>
+  );
 }
