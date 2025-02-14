c18ead14efce7371c44c675bfd0de37262c9588c
puck3 <shipilovk04@gmail.com>
2025-02-14 22:43:12 +0300

    add info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 "use client";
 
-import Contacts from "@/components/Contacts";
 import ExamplesGallery from "@/components/ExamplesGallery";
 import FAQ from "@/components/FAQ";
+import ServiceInfo from "@/components/Info";
 import ServicesList from "@/components/Services/ServicesList";
 
 export default function Home() {
   return (
     <main className="text-gray-900">
-      {/* <h1 className="my-[2rem] text-xl md:text-2xl lg:text-4xl font-bold text-center">
-        Химчистка диванов и мягкой мебели
-      </h1> */}
+      <ServiceInfo />
 
+      {/* TODO: move sections inside components */}
       <section id="services" className="my-[2rem] scroll-mt-[5rem]">
-        <h2 className="my-[1rem] text-xl md:text-2xl lg:text-3xl font-bold">
+        <h1 className="my-[1rem] text-xl md:text-2xl lg:text-3xl font-bold">
           Наши услуги
-        </h2>
+        </h1>
         <ServicesList />
       </section>
 
