7f28a5bbd460b25b724d9656531650ab50d7d1e0
puck3 <shipilovk04@gmail.com>
2025-02-21 13:57:59 +0300

    rename cart store

--- src/components/Services/CardButton.tsx
+++ src/components/Services/CardButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import useCartStore from "@/store/useCartStore";
+import useCartStore from "@/store/CartStore";
 import Service from "@/types/Service";
 import { useShallow } from "zustand/shallow";
 
