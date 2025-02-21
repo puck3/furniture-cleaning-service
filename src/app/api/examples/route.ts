5ef08a9b99e6a9663ae06d647ca41b31c1bb3f37
puck3 <shipilovk04@gmail.com>
2025-02-21 13:14:19 +0300

    refactor examples api

--- src/app/api/examples/route.ts
+++ src/app/api/examples/route.ts
@@ -1,41 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
-import { google } from "googleapis";
+import { drive_v3, google } from "googleapis";
 
-export async function GET(req: NextRequest) {
+const getAuth = () => {
+  const credentials = process.env.GOOGLE_CREDS;
+  if (!credentials) {
+    throw new Error("Google credentials not found");
+  }
+  return new google.auth.GoogleAuth({
+    credentials: JSON.parse(credentials),
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  });
+};
+
+const getQueryParams = () => {
+  const folderId = process.env.FOLDER_ID;
+  if (!folderId) {
+    throw new Error("Folder ID is not set in environment variables");
+  }
+  return {
+    q: `'${folderId}' in parents`,
+    fields: "files(id)",
+  };
+};
+
+const fetchUrls = async (
+  auth: drive_v3.Params$Resource$Files$List["auth"],
+  params: drive_v3.Params$Resource$Files$List
+): Promise<string[]> => {
+  const drive = google.drive({ version: "v3", auth });
+  const response = await drive.files.list(params);
+  const files = response.data.files || [];
+  return files.map((file) => `https://drive.google.com/uc?id=${file.id}`);
+};
+
+export const GET = async (req: NextRequest) => {
   try {
-    const credentials = process.env.GOOGLE_CREDS;
-    if (!credentials) {
-      return NextResponse.json(
-        { error: "Google credentials not found" },
-        { status: 500 }
-      );
-    }
-
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(credentials),
-      scopes: ["https://www.googleapis.com/auth/drive.readonly"],
-    });
-
-    const drive = google.drive({ version: "v3", auth });
-
-    const folderId = process.env.FOLDER_ID;
-    if (!folderId) {
-      return NextResponse.json(
-        { error: "Folder ID is not set in environment variables" },
-        { status: 400 }
-      );
-    }
-
-    const response = await drive.files.list({
-      q: `'${folderId}' in parents`,
-      fields: "files(id, name)",
-    });
-
-    const files = response.data.files || [];
-    const fileUrls = files.map(
-      (file) => `https://drive.google.com/uc?id=${file.id}`
-    );
-
+    const auth = getAuth();
+    const params = getQueryParams();
+    const fileUrls = await fetchUrls(auth, params);
     return NextResponse.json(fileUrls);
   } catch (error) {
     console.error("Ошибка при загрузке файлов:", error);
@@ -44,4 +46,4 @@ export async function GET(req: NextRequest) {
       { status: 500 }
     );
   }
-}
+};
